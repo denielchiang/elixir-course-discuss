dd51a6832fee139eaffeaf981648df48614563db
Deniel Chiang <deniel@cogini.com>
2018-05-04 02:12:05 +0800

    CRUD done

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -2,21 +2,23 @@ defmodule DiscussWeb.Router do
   use DiscussWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", DiscussWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get "/", TopicController, :index
+    resources "/topics/", TopicController
   end
 
   # Other scopes may use custom stacks.
